afe63d015def43f41b67d886a8f4562a2d6d967a
Adrián.Álvarez <adrian.alvarez@eusa.es>
2025-11-18 11:56:06 +0100

    last changes

--- bienvenida.php
+++ bienvenida.php
@@ -8,5 +8,30 @@
         <h2>Bienvenida</h2>
         <div id ="informacion"></div>
 
+        <button onclick="cerrarSesion()">Cerrar Sesión</button>
+
+        <script>
+            const token = localStorage.getItem("token");
+
+            if (!token){
+                window.location.href = "noAcceso.php";
+            }
+
+            fetch("api/welcome.php", {
+                headers: {
+                    "Autorization" : "Bearer" + token
+                }
+            })
+            .then(async response => {
+                if(response.status === 403){
+                    window.location.href = "noAcceso.php";
+                }
+
+                const data = await response.json();
+                document.getElementById("informacion").innerHTML =
+                    
+            })
+
+
         
 </html>
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+    $usuarios = [
+        ["username" => "admin", "password" => "1234"],
+        ["username" => "Adrián", "password" => "12345"]
+    ];
     header("Location: login.php");
     exit();
     ?>
